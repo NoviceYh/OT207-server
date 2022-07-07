460b505e95e7cdd4c26c4881239dcfb60e7f5e22
Alejandro Aguirre <aguirreee21@gmail.com>
2022-07-07 18:49:35 -0300

    create givenUpdatedActivity_whenUpdateActivity_thenReturnIsAccepted and change other methods names

--- src/test/java/com/alkemy/ong/controller/ActivityControllerTests.java
+++ src/test/java/com/alkemy/ong/controller/ActivityControllerTests.java
@@ -3,11 +3,13 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.auth.service.JwtUtils;
 import com.alkemy.ong.auth.service.impl.UserDetailsCustomService;
 import com.alkemy.ong.domain.service.IActivityService;
+import com.alkemy.ong.domain.util.JsonUtils;
 import com.alkemy.ong.domain.util.Url;
 import com.alkemy.ong.dto.ActivityDTO;
 import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.dto.CategoryDtoName;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +23,10 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -60,7 +64,7 @@ class ActivityControllerTests {
 
     @Test
     @WithMockUser(setupBefore = TestExecutionEvent.TEST_METHOD, username = "Alejandro")
-    void testCreateCategoryWithNotRights() throws Exception {
+    void testCreateActivityWithNotRights() throws Exception {
         ActivityDTO activityDTO = createDtoEntity();
 
         given(activityService.save(any(ActivityDTO.class)))
@@ -78,7 +82,7 @@ class ActivityControllerTests {
 
     @Test
     @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
-    void testCreateCategory() throws Exception {
+    void testCreateActivity() throws Exception {
         ActivityDTO activityDTO = createDtoEntity();
 
         given(activityService.save(any(ActivityDTO.class)))
@@ -98,6 +102,22 @@ class ActivityControllerTests {
                         is(activityDTO.getImage())))
                 .andExpect(jsonPath("$.content",
                         is(activityDTO.getContent())));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+    void givenUpdatedActivity_whenUpdateActivity_thenReturnIsAccepted() throws Exception {
+        long activityId = 99L;
+        ActivityDTO updateActivityDTO = createDtoEntity();
+        given(activityService.update(eq(activityId), any(ActivityDTO.class))).willReturn(updateActivityDTO);
+        mockMvc.perform(put(Url.ACTIVITIES_URI + "/" + activityId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(updateActivityDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", CoreMatchers.is(updateActivityDTO.getName())))
+                .andExpect(jsonPath("$.image", CoreMatchers.is(updateActivityDTO.getImage())))
+                .andExpect(jsonPath("$.content", CoreMatchers.is(updateActivityDTO.getContent())))
+                .andDo(print());
     }
 
 }
