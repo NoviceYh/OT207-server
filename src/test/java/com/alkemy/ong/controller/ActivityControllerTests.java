d38b06bfbb760547c438cc827e0955ab4b12319e
Alejandro Aguirre <aguirreee21@gmail.com>
2022-07-07 18:43:31 -0300

    create testCreateCategoryWithNotRights

--- src/test/java/com/alkemy/ong/controller/ActivityControllerTests.java
+++ src/test/java/com/alkemy/ong/controller/ActivityControllerTests.java
@@ -3,13 +3,29 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.auth.service.JwtUtils;
 import com.alkemy.ong.auth.service.impl.UserDetailsCustomService;
 import com.alkemy.ong.domain.service.IActivityService;
-import com.alkemy.ong.domain.service.ICategoryService;
+import com.alkemy.ong.domain.util.Url;
+import com.alkemy.ong.dto.ActivityDTO;
+import com.alkemy.ong.dto.CategoryDTO;
+import com.alkemy.ong.dto.CategoryDtoName;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ActivityController.class)
 public class ActivityControllerTests {
@@ -33,20 +49,32 @@ public class ActivityControllerTests {
         objectMapper = new ObjectMapper();
     }
 
-    /*
-    * private CategoryDTO createDtoEntity() {
-        CategoryDTO categoryDTO = new CategoryDTO();
-        categoryDTO.setName("News");
-        categoryDTO.setImage("image.png");
-        categoryDTO.setDescription("This is a description");
-        return categoryDTO;
+
+     private ActivityDTO createDtoEntity() {
+        ActivityDTO activityDTO = new ActivityDTO();
+        activityDTO.setName("Activity");
+        activityDTO.setImage("image.jpg");
+        activityDTO.setContent("content");
+        return activityDTO;
     }
 
-    private CategoryDtoName createDtoNameEntity() {
-        CategoryDtoName categoryDTO = new CategoryDtoName();
-        categoryDTO.setName("News");
-        return categoryDTO;
+    @Test
+    @WithMockUser(setupBefore = TestExecutionEvent.TEST_METHOD, username = "Alejandro")
+    void testCreateCategoryWithNotRights() throws Exception {
+        ActivityDTO activityDTO = createDtoEntity();
+
+        given(activityService.save(any(ActivityDTO.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        String activityString = objectMapper.writeValueAsString(activityDTO);
+
+        ResultActions response = mockMvc.perform(post(Url.ACTIVITIES_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(activityString));
+
+        response.andDo(print()).
+                andExpect(status().isForbidden());
     }
-    * */
+
 
 }
